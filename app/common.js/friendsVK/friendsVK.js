d98259809e3a5ed7bbacefe64fd957a581ecc362
Максим Cтуденикин <operatoribm@gmail.com>
2018-08-25 20:45:36 +0300

    vk init

--- app/common.js/friendsVK/friendsVK.js
+++ app/common.js/friendsVK/friendsVK.js
@@ -1,17 +1,18 @@
-VK.init({
-  apiId: 6669907
-});
 
-function auth() {
-  return Promise((resolve, reject) => {
-    VK.Auth.login(data => {
-      if (data.session) {
-        resolve();
-      } else {
-        reject(new Error('Все плохо'));
-      }
-    }, 2);
-  });
-}
 
-auth().then(() => console.log('live is good'));
+// function auth() {
+//   return Promise((resolve, reject) => {
+//     VK.init({
+//       apiId: 6669907
+//     });
+//     VK.Auth.login(data => {
+//       if (data.session) {
+//         resolve();
+//       } else {
+//         reject(new Error('Все плохо'));
+//       }
+//     }, 2);
+//   });
+// }
+
+// auth().then(() => console.log('live is good'));
--- app/common.js/index.js
+++ app/common.js/index.js
@@ -9,23 +9,28 @@ import '../common.blocks/index.scss';
 // import { auth } from './friendsVK/friendsVK';
 
 
+
 VK.init({
   apiId: 6669907
-});
+  })
+  
+  function auth() {
+    return Promise((resolve, reject) => {
+      VK.Auth.login(data => {
+        if (data.session) {
+          resolve();
+        } else {
+          reject(new Error('Все плохо'));
+        }
+      }, 2);
+    });
+  }
 
-function auth() {
-  return Promise((resolve, reject) => {
-    VK.Auth.login(data => {
-      if (data.session) {
-        resolve();
-      } else {
-        reject(new Error('Все плохо'));
-      }
-    }, 2);
-  });
-}
+  auth().then(() => console.log('live is good'));
 
-auth().then(() => console.log('live is good'));
+
+
+
 
 
 // var doc = document;
